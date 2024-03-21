78c5828a4128f4733072b8379deed220376757af
cnrdh <cnrdh@users.noreply.github.com>
2024-03-21 12:08:25 +0100

    add KV

--- kv.ts
+++ kv.ts
@@ -1,7 +1,7 @@
 import { Akvaplanist, ExpiredAkvaplanist } from "./types.ts";
 
 export const kv = await Deno.openKv(
-  "https://api.deno.com/databases/4d8b08fa-92cc-4f38-9abd-ac60b6e755c9/connect",
+  //"https://api.deno.com/databases/4d8b08fa-92cc-4f38-9abd-ac60b6e755c9/connect",
 );
 
 export const person0 = "person";
