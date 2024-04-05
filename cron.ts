9372face95cd1696eedde146eb8f9c9b7a6d5881
cnrdh <cnrdh@users.noreply.github.com>
2024-04-05 10:46:45 +0200

    reuse kv

--- cron.ts
+++ cron.ts
@@ -1,8 +1,7 @@
-import { kv } from "./kv.ts";
 import { fetchAndIngestAkvaplanists } from "./fetch_and_ingest.ts";
 
 Deno.cron(
   "Refresh akvaplanists",
   "16 15 * * *",
-  async () => await fetchAndIngestAkvaplanists(kv),
+  async () => await fetchAndIngestAkvaplanists(),
 );
