b8cec450cd24e480475f049c2783b334b892c021
cnrdh <cnrdh@users.noreply.github.com>
2024-04-09 12:04:17 +0200

    remove 2 patches

--- patches.ts
+++ patches.ts
@@ -1,9 +1,8 @@
 export const patches = new Map([
   ["aen", { management: true }],
   ["odj", { given: "Ólöf Dóra Bartels", family: "Jónsdóttir" }],
-  ["aki", { given: "Albert K. D." }], // Kjartan Dagbjartarson
+  //["aki", { given: "Albert K. D." }],
   ["skc", { from: new Date("1992-01-01T12:00:00Z") }],
   ["ref", { section: "INNOV" }],
-  ["kkr", { workplace: "Tromsø" }],
   ["per", { given: "Paul E." }],
 ]);
