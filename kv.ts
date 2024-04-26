4364ea591ec62dc22fbbb55b17932f2ca5e97b1b
cnrdh <cnrdh@users.noreply.github.com>
2024-04-26 14:15:56 +0200

    require valid

--- kv.ts
+++ kv.ts
@@ -57,36 +57,36 @@ export const setAkvaplanistTx = async (
     console.error(
       JSON.stringify({ error: { input: akvaplanist, messages } }),
     );
-  }
-  const { id, expired } = akvaplanist;
-  const key = [person0, id];
-
-  if (expired && new Date() >= new Date(expired)) {
-    // BEGIN
-    const atomic = kv.atomic();
-
-    // DELETE regular record
-    console.warn("DELETE", key);
-    atomic.delete(key);
-
-    // INSERT expired record
-    const expkey = [expired0, id];
-    const minimal = toExpired(akvaplanist);
-    console.warn("INSERT", expkey, minimal);
-    atomic.check({ key, versionstamp: null })
-      .set(expkey, minimal);
-
-    // COMMIT
-    await atomic.commit();
   } else {
-    tx.set(key, akvaplanist);
+    const { id, expired } = akvaplanist;
+    const key = [person0, id];
+
+    if (expired && new Date() >= new Date(expired)) {
+      // BEGIN
+      const atomic = kv.atomic();
+
+      // DELETE regular record
+      console.warn("DELETE", key);
+      atomic.delete(key);
+
+      // INSERT expired record
+      const expkey = [expired0, id];
+      const minimal = toExpired(akvaplanist);
+      console.warn("INSERT", expkey, minimal);
+      atomic.check({ key, versionstamp: null })
+        .set(expkey, minimal);
+
+      // COMMIT
+      await atomic.commit();
+    } else {
+      tx.set(key, akvaplanist);
+    }
   }
 };
 
 export const setAkvaplanists = async (chunk: Akvaplanist[]) => {
   const tx = kv.atomic();
   for await (const akvaplanist of chunk) {
-    //console.debug(akvaplanist);
     await setAkvaplanistTx(akvaplanist, tx);
   }
   const { ok } = await tx.commit();
