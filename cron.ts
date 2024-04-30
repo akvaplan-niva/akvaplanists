a6eca688ee1d5cc97115073847f341857e63a38a
cnrdh <cnrdh@users.noreply.github.com>
2024-04-30 09:11:11 +0200

    earlier refresh

--- cron.ts
+++ cron.ts
@@ -2,6 +2,6 @@ import { fetchAndIngestAkvaplanists } from "./fetch_and_ingest.ts";
 
 Deno.cron(
   "Refresh akvaplanists",
-  "16 15 * * *",
+  "7 15 * * *",
   async () => await fetchAndIngestAkvaplanists(),
 );
