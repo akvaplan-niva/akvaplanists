c07fd7cf12eab23a9e344ceecc232844b68a9325
cnrdh <cnrdh@users.noreply.github.com>
2024-09-23 09:57:20 +0200

    rename

---
+++ migrate/2024-09-16_replace_priors.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S deno run --env-file --allow-env --allow-net
+
+import { kv } from "../kv.ts";
+import priors from "../data/priors.json" with { type: "json" };
+// ulid
+const replacePriors = async (id?: string) => {
+  for await (let prior of priors) {
+    const key = ["expired", prior.id];
+    const { value } = await kv.get(key);
+    if (value) {
+      prior = { ...value, ...prior };
+    }
+    prior.updated = new Date();
+    await kv.set(key, prior);
+  }
+};
+
+const replacePrior = async (id: string) => {
+  const prior = priors.find((p) => p.id === id);
+  if (prior) {
+    const key = ["expired", prior.id];
+    prior.updated = new Date();
+    await kv.set(key, prior);
+  }
+};
+
+if (import.meta.main) {
+  const [id] = Deno.args;
+  if (id) {
+    replacePrior(id);
+  } else {
+    replacePriors();
+  }
+}
