4af473d4168d16e622b2fc45527b0dd948ebe8ee
cnrdh <cnrdh@users.noreply.github.com>
2024-12-06 12:18:26 +0100

    disable

--- cron.ts
+++ cron.ts
@@ -1,7 +1,9 @@
 import { fetchAndIngestAkvaplanists } from "./fetch_and_ingest.ts";
 
+/**
 Deno.cron(
   "Refresh akvaplanists",
   "47 9 * * *",
   async () => await fetchAndIngestAkvaplanists(),
 );
+*/
