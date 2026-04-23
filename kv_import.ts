67e2ac09f2ac16f495c49c19dd07c3def86cfd2e
cnrdh <cnrdh@users.noreply.github.com>
2026-04-23 08:53:22 +0200

    await

--- kv_import.ts
+++ kv_import.ts
@@ -2,15 +2,19 @@ import { importEntries } from "@deno/kv-utils";
 
 const importIntoKv = async (url: string) => {
   const r = await fetch(url);
-  const options = {
-    overwrite: false,
-    prefix: [],
-    onError: (e: unknown) => console.error(e),
-    onProgress: (count: number, skipped: number, errors: number) =>
-      console.warn({ count, skipped, errors }),
-  };
+
   if (r.ok && r.body) {
     using kv = await Deno.openKv();
+
+    const options = {
+      overwrite: false,
+      prefix: [],
+      onError: (e: unknown) => console.error(e),
+      onProgress: (count: number, skipped: number, errors: number) => {
+        console.warn({ count, skipped, errors });
+      },
+    };
+
     const result = await importEntries(kv, r.body, options);
     console.warn(result);
     console.assert(result.errors === 0);
@@ -19,6 +23,6 @@ const importIntoKv = async (url: string) => {
 
 if (import.meta.main) {
   if (Deno.env.has("KV_IMPORT_URL")) {
-    importIntoKv(Deno.env.get("KV_IMPORT_URL")!);
+    await importIntoKv(Deno.env.get("KV_IMPORT_URL")!);
   }
 }
