c95116872d821434b4da95853997f677292ac895
cnrdh <cnrdh@users.noreply.github.com>
2026-04-24 11:56:57 +0200

    kv import from plain ndjson

--- kv_import.ts
+++ kv_import.ts
@@ -1,27 +1,39 @@
-import { importEntries } from "@deno/kv-utils";
+const kv = await Deno.openKv();
 
 const importIntoKv = async (url: string) => {
+  const last = await kv.get<bigint>(["kv_import"]);
+  const status = { insert: 0, missing: 0, found: 0, lines: 0 };
+  if (last.value) {
+    const inst = new Temporal.Instant(last.value);
+    console.warn("Last", inst.toZonedDateTimeISO("UTC"));
+  }
   const r = await fetch(url);
-
   if (r.ok && r.body) {
-    const kv = await Deno.openKv();
-
-    const options = {
-      overwrite: false,
-      // onError: (e: unknown) => console.error(e),
-      // onProgress: (count: number, skipped: number, errors: number) => {
-      //   console.warn({ count, skipped, errors });
-      // },
-    };
-
-    const result = await importEntries(kv, r.body, options);
-    //console.warn(result);
-    console.assert(result.errors === 0);
+    const text = await r.text();
+    const lines = text.trim().split("\n");
+    for await (const line of lines) {
+      ++status.lines;
+      const { key, value } = JSON.parse(line);
+      const maybeEntry = await kv.get(key);
+      if (maybeEntry.versionstamp) {
+        ++status.found;
+        //console.log("In KV", key);
+      } else {
+        console.warn("Missing", { key, value });
+        ++status.missing;
+        const res = await kv.set(key, value);
+        if (res.ok) {
+          ++status.insert;
+        }
+      }
+    }
+    console.warn(status);
   }
 };
-
 if (import.meta.main) {
   if (Deno.env.has("KV_IMPORT_URL")) {
     await importIntoKv(Deno.env.get("KV_IMPORT_URL")!);
+    const now = Temporal.Now.instant();
+    await kv.set(["kv_import"], now.epochNanoseconds);
   }
 }
