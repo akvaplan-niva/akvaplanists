99cbc2190543e38547c19c8956a447bfea4c75ee
cnrdh <cnrdh@users.noreply.github.com>
2026-04-24 12:08:47 +0200

    remove unused kv export

--- kv_export.ts
+++
@@ -1,7 +0,0 @@
-import { exportEntries } from "@deno/kv-utils";
-
-using kv = await Deno.openKv();
-using file = await Deno.open("./data/kv.ndjson", { write: true, create: true });
-for await (const chunk of exportEntries(kv, { prefix: [] })) {
-  await file.write(chunk);
-}
