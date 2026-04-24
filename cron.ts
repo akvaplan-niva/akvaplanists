8e9d1b6a99ef7f08963212bf0d29223f3c011365
cnrdh <cnrdh@users.noreply.github.com>
2026-04-24 13:33:16 +0200

    move kv import to cron

--- cron.ts
+++ cron.ts
@@ -1,7 +1,18 @@
 import { fetchAndIngestAkvaplanists } from "./fetch.ts";
+import { importIntoKv } from "./kv_import.ts";
 
 Deno.cron(
   "Refresh akvaplanists",
   "42 8 * * *",
   async () => await fetchAndIngestAkvaplanists(),
+);
+
+Deno.cron(
+  "Import akvaplanists",
+  "36 13 * * *",
+  async () => {
+    if (Deno.env.has("KV_IMPORT_URL")) {
+      await importIntoKv(Deno.env.get("KV_IMPORT_URL")!);
+    }
+  },
 );
